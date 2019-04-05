2dcd339616c26e7be8d0abcb2b63d06f9fcc00ec
sunset wan <sunsetwan@gmail.com>
2019-04-05 16:07:33 +0800

    refine comments

--- Concentration/ConcentrationThemeChooserViewController.swift
+++ Concentration/ConcentrationThemeChooserViewController.swift
@@ -16,14 +16,45 @@ class ConcentrationThemeChooserViewController: UIViewController {
         "Faces": "😀😌😎🤓😠😤😭😰😱😳😜😇"
     ]
 
+    // Why do we need performSegue mannually instead of ctrl-dragging in storyboard?
+    // Because someone may want conditional segueing.
+    @IBAction func changeTheme(_ sender: Any) {
+        // how to find a splitView
+        if let cvc = splitDetailConcentrationViewController {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                cvc.theme = theme
+            }
+            // how to hold something in the heap that gets thrown off of a navigation stack
+        }else if let cvc = lastSeguedToConcentrationViewController {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                cvc.theme = theme
+            }
+            // how to push things on a navigation stack without segueing
+            navigationController?.pushViewController(cvc, animated: true)
+        } else {
+            // how to segue from code
+                performSegue(withIdentifier: "Choose Theme", sender: sender)
+            }
+
+        
+    }
+    
+    // On an non-Plus iPhone, there isn't a splitViewController.
+    private var splitDetailConcentrationViewController: ConcentrationViewController? {
+        return splitViewController?.viewControllers.last as? ConcentrationViewController
+    }
     
     // MARK: - Navigation
+    
+    private var lastSeguedToConcentrationViewController: ConcentrationViewController?
+    
     // It's crucial to understand that this preparation is happening BEFORE outlets get set!
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "Choose Theme" {
             if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
                 if let cvc = segue.destination as? ConcentrationViewController {
                     cvc.theme = theme
+                    lastSeguedToConcentrationViewController = cvc
                 }
             }
         }
