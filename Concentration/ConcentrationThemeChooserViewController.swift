9db4aa4e8d5b39254856b0ecc1c905a771d748a1
sunset wan <sunsetwan@gmail.com>
2019-04-05 15:02:00 +0800

    add Navigation Controller

--- Concentration/ConcentrationThemeChooserViewController.swift
+++ Concentration/ConcentrationThemeChooserViewController.swift
@@ -10,21 +10,27 @@ import UIKit
 
 class ConcentrationThemeChooserViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    let themes = [
+        "Sports": "⚽️🏀🏈⚾️🎾🏐🏉🎱🏓⛷🎳⛳️",
+        "Animals": "🐶🦆🐹🐸🐘🦍🐓🐩🐦🦋🐙🐏",
+        "Faces": "😀😌😎🤓😠😤😭😰😱😳😜😇"
+    ]
 
-        // Do any additional setup after loading the view.
-    }
     
-
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "Choose Theme" {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                if let cvc = segue.destination as? ConcentrationViewController {
+                    cvc.theme = theme
+                }
+            }
+        }
     }
-    */
+
+    
+    
+ 
 
 }
